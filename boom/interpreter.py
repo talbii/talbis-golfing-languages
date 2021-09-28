7c0c70476e754a9baee8841ec3a2cd716a19bc00
talbii <talbioffical@gmail.com>
2021-09-28 20:10:39 +0300

    Boom Python implementation

---
+++ boom/interpreter.py
@@ -0,0 +1,28 @@
+import sys
+
+def Halt() -> None:
+    """ Calling this function will make the program halt. """
+    while(True):
+        pass
+
+def main():
+    """ Boom main implementation"""
+    if(len(sys.argv) < 2):
+        print("[Boom] Interpreting implicit empty source file.")
+        Halt()
+    try:
+        with open(sys.argv[1], "r") as f:
+            program = f.read()
+    except FileNotFoundError:
+        print("[Boom] Error opening file.")
+        return
+    
+    if(program != ""):
+        print(f"[Boom] Invalid syntax: character `{program[0]}` at Line 1:1\n>    Perhaps try an empty program instead?")
+        return
+    
+    Halt()
+
+
+if(__name__ == "__main__"):
+    main()
